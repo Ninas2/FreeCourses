199fbdf577d43e3117f2ffd0979c5db2d1831c38
Konstantinos Ninas <104201293+Ninas2@users.noreply.github.com>
2023-03-16 20:10:53 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -5,6 +5,8 @@ import streamlit as st
 from bs4 import BeautifulSoup
 
 
+pip install --upgrade pip
+
 st.title('Udemy Free Courses')
 
 url = 'https://www.udemy.com/courses/free/'
@@ -34,6 +36,8 @@ response = requests.get(url)
         # If the request was unsuccessful, print an error message
         print('Error: Could not retrieve data from Udemy.')
         return None
+       
+       
 # json_data = json.loads(response.text)
 
 # courses = []
@@ -49,59 +53,3 @@ response = requests.get(url)
 
 
 
-
-# import requests
-# from bs4 import BeautifulSoup
-# import pandas as pd
-# import streamlit as st
-
-# # Scraping function
-# def scrape_udemy_free_courses():
-#     # Udemy free courses URL
-#     url = 'https://www.udemy.com/courses/free/'
-    
-#     # Send a GET request to the URL
-#     response = requests.get(url)
-    
-#     # Check if the request was successful
-#     if response.status_code == 200:
-#         # Parse the HTML content using BeautifulSoup
-#         soup = BeautifulSoup(response.content, 'html.parser')
-        
-#         # Find all the free course titles and links on the page
-#         course_titles = []
-#         course_links = []
-#         for course in soup.find_all('div', {'class': 'course-list--container--3zXPS'}):
-#             title = course.find('div', {'class': 'course-list--course-title--2f7tE'}).text.strip()
-#             link = 'https://www.udemy.com' + course.find('a')['href']
-#             course_titles.append(title)
-#             course_links.append(link)
-        
-#         # Create a pandas dataframe to store the course titles and links
-#         df = pd.DataFrame({'Course Title': course_titles, 'Course Link': course_links})
-        
-#         # Return the dataframe
-#         return df
-    
-#     else:
-#         # If the request was unsuccessful, print an error message
-#         print('Error: Could not retrieve data from Udemy.')
-#         return None
-
-# # Streamlit app code
-# def app():
-#     st.title('Free Courses on Udemy')
-#     st.write('This app scrapes all the free courses on Udemy.')
-    
-#     # Scrape the free courses data
-#     df = scrape_udemy_free_courses()
-    
-#     # Check if the data was successfully retrieved
-#     if df is not None:
-#         st.write('Total Courses:', len(df))
-#         st.table(df)
-#     else:
-#         st.write('Error: Could not retrieve data from Udemy.')
-        
-# if __name__ == '__main__':
-#     app()
