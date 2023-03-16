e82ee41a4cee2566685d339f5b9df7ea0b86c53a
Konstantinos Ninas <104201293+Ninas2@users.noreply.github.com>
2023-03-16 20:11:35 +0200

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -5,37 +5,37 @@ import streamlit as st
 from bs4 import BeautifulSoup
 
 
-pip install --upgrade pip
 
-st.title('Udemy Free Courses')
 
-url = 'https://www.udemy.com/courses/free/'
+# st.title('Udemy Free Courses')
 
-response = requests.get(url)
- # Check if the request was successful
-    if response.status_code == 200:
-        # Parse the HTML content using BeautifulSoup
-        soup = BeautifulSoup(response.content, 'html.parser')
+# url = 'https://www.udemy.com/courses/free/'
+
+# response = requests.get(url)
+#  # Check if the request was successful
+#     if response.status_code == 200:
+#         # Parse the HTML content using BeautifulSoup
+#         soup = BeautifulSoup(response.content, 'html.parser')
         
-        # Find all the free course titles and links on the page
-        course_titles = []
-        course_links = []
-        for course in soup.find_all('div', {'class': 'course-list--container--3zXPS'}):
-            title = course.find('div', {'class': 'course-list--course-title--2f7tE'}).text.strip()
-            link = 'https://www.udemy.com' + course.find('a')['href']
-            course_titles.append(title)
-            course_links.append(link)
+#         # Find all the free course titles and links on the page
+#         course_titles = []
+#         course_links = []
+#         for course in soup.find_all('div', {'class': 'course-list--container--3zXPS'}):
+#             title = course.find('div', {'class': 'course-list--course-title--2f7tE'}).text.strip()
+#             link = 'https://www.udemy.com' + course.find('a')['href']
+#             course_titles.append(title)
+#             course_links.append(link)
 
- # Create a pandas dataframe to store the course titles and links
-        df = pd.DataFrame({'Course Title': course_titles, 'Course Link': course_links})
+#  # Create a pandas dataframe to store the course titles and links
+#         df = pd.DataFrame({'Course Title': course_titles, 'Course Link': course_links})
         
-        # Return the dataframe
-        return st.write(df)
+#         # Return the dataframe
+#         return st.write(df)
     
-    else:
-        # If the request was unsuccessful, print an error message
-        print('Error: Could not retrieve data from Udemy.')
-        return None
+#     else:
+#         # If the request was unsuccessful, print an error message
+#         print('Error: Could not retrieve data from Udemy.')
+#         return None
        
        
 # json_data = json.loads(response.text)
